18a46a263ca644f07e0c2c00a6d016cd344c8d83
Joshua Schlueter <contact@joshuaschlueter.com>
2021-11-18 18:32:09 -0600

    Reworked functions around Nick's recommendations

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 //target icons and add listeners to run click function with respective 
-const Meta = document.querySelector("#Meta-Logo")
-Meta.addEventListener("click",clickImg("FB"))
+const imgBar = document.querySelector("#stock-menu")
+Meta.addEventListener("click", (e) => clickImg(e))
 
-const Apple = document.querySelector("#Apple-Logo")
-Apple.addEventListener("click",clickImg("AAPL"))
 
-const Amazon = document.querySelector("#Amazon-Logo")
-Amazon.addEventListener("click",clickImg("AMZN"))
 
-const Netflix = document.querySelector("#Netflix-Logo")
-Netflix.addEventListener("click",clickImg("NFLX"))
-
-const Alphabet = document.querySelector("#Google-Logo")
-Alphabet.addEventListener("click",clickImg("GOOG"))
-
-function clickImg(ticker) {
+function clickImg(e) {
     //create a fetch dynamically using the ticker of the stock that was clicked
-    fetch(`https://api.stockdata.org/v1/data/quote?symbols=${ticker}%2C&api_token=8wL6E8iXNf1JvxVu9NoYPCeIVaZ7qJpgGCu46QdQ`)
+    fetch(`https://api.stockdata.org/v1/data/quote?symbols=${e.target.dataset.symbol}%2C&api_token=8wL6E8iXNf1JvxVu9NoYPCeIVaZ7qJpgGCu46QdQ`)
     .then(res => res.json())
     .then(quote => renderQuote(quote.data))
 
@@ -35,22 +25,23 @@ function clickImg(ticker) {
     //Updates placeholder img with the logo of the stock being quoted
     const quoteIcon = document.querySelector(".quote-image")
     //this is how I think the img would be updated but need to work out targeting the proper image
-    // quoteIcon.src = ticker.src
+    quoteIcon.src = e.target.src
+
+    
+    fetch(`https://api.stockdata.org/v1/entity/profile?symbols=${e.target.dataset.symbol}&api_token=8wL6E8iXNf1JvxVu9NoYPCeIVaZ7qJpgGCu46QdQ`)
+    .then(res => res.json())
+    .then(companyProfile => () => renderProfile(companyProfile.data))
+    function renderProfile() {
+    const companyBio = document.querySelector(".company-bio")
+    companyBio.innerText = profile.description
+    }
 }
 
 
 
-// const profileBttn = document.querySelector(".profile-button")
-// profileBttn.addEventListener("click", renderProfile)
+const profileBttn = document.querySelector(".profile-button")
+profileBttn.addEventListener("click", () => unhideBio)
 
+function unhideBio(){
 
-
-// function renderProfile(profile) {
-
-//     // fetch("https://api.stockdata.org/v1/entity/profile?symbols=AAPL&api_token=8wL6E8iXNf1JvxVu9NoYPCeIVaZ7qJpgGCu46QdQ")
-//     // .then(res => res.json())
-//     // .then(companyProfile => populate(companyProfile.data))
-
-//     const companyBio = document.querySelector(".company-bio")
-//     companyBio.innerText = profile.description
-// }
+}
